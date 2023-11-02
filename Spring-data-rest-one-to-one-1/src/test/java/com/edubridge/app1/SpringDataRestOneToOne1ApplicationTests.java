94c86d64b1a136950ef40e3b8119ee91e9479638
SindhumsSindhu <138276765+SindhumsSindhu@users.noreply.github.com>
2023-11-02 16:01:32 +0530

    Add files via upload

---
+++ Spring-data-rest-one-to-one-1/src/test/java/com/edubridge/app1/SpringDataRestOneToOne1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edubridge.app1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRestOneToOne1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
