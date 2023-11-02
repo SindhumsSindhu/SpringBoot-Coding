4a6fb95b064db562326da96a27cb527428d97f6c
SindhumsSindhu <138276765+SindhumsSindhu@users.noreply.github.com>
2023-11-02 15:58:08 +0530

    Add files via upload

---
+++ spring-data-rest-demo/src/main/java/com/edubridge/app1/SpringDataRestDemoApplication.java
@@ -0,0 +1,14 @@
+package com.edubridge.app1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataRestDemoApplication.class, args);
+		
+	}
+
+}
---
+++ spring-data-rest-demo/src/main/java/com/edubridge/app1/nnt/Employee.java
@@ -0,0 +1,56 @@
+package com.edubridge.app1.nnt;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+
+import lombok.Getter;
+
+import lombok.NoArgsConstructor;
+
+import lombok.Setter;
+
+import lombok.ToString;
+
+ 
+
+@Getter
+
+@Setter
+
+@NoArgsConstructor
+
+@AllArgsConstructor
+
+@ToString
+
+@Entity
+
+public class Employee {
+
+ 
+
+	@Id
+
+	@GeneratedValue
+
+	private Integer employeeId;
+
+	private String employeeName;
+
+	private double employeeSalary;
+
+	private String employeeEmail;
+
+	private Long employeeMobile;
+
+	
+
+}
+
+
+
+	
+	
+
+
